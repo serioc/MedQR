3925f3b86261987dbae0425e27f008c000881e78
Enea Milo <miloe@wit.edu>
2018-06-10 22:25:22 -0400

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="assets/css/register_style.css">
+<?php session_start(); ?>
+<div class="body content">
+    <div class="welcome">
+        <div class="alert alert-success"><?= $_SESSION['message'] ?></div>
+        Welcome <span class="user"><?= $_SESSION['firstname'] ?></span>
+        <?php
+        $mysqli = new mysqli("localhost1234", "root", "Shqiperi1!", "user accounts");
+        //Select queries return a resultset
+        $sql = "SELECT firstname FROM users";
+        $result = $mysqli->query($sql); //$result = mysqli_result object
+        //var_dump($result);
+        ?>
+    </div>
+</div>
